5d8fdfdb2f267981fb973ddd1336ccc238e84620
Pedro <pedrohalmeidamendonca@gmail.com>
2022-11-03 17:48:12 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,42 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    buttonDisabled: true,
+    inputText: '',
+  };
+
+  checkInput = ({ target: { value } }) => {
+    const two = 2;
+    if (value.length >= two) {
+      this.setState({
+        buttonDisabled: false,
+      });
+    }
+    this.setState({
+      inputText: value,
+    });
+  };
+
   render() {
+    const { buttonDisabled, inputText } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Search</p>
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          name="inputText"
+          value={ inputText }
+          onChange={ this.checkInput }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ buttonDisabled }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
