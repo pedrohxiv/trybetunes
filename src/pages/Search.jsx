a6c00fe50216d3be3a605570b9020aeeee4d47b8
Pedro <pedrohalmeidamendonca@gmail.com>
2022-11-03 22:36:53 -0300

    Requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 export default class Search extends Component {
   state = {
     buttonDisabled: true,
     inputText: '',
+    albums: [],
+    artist: '',
   };
 
   checkInput = ({ target: { value } }) => {
@@ -19,8 +23,17 @@ export default class Search extends Component {
     });
   };
 
+  searchAlbum = async () => {
+    const { inputText } = this.state;
+    this.setState({
+      inputText: '',
+      albums: await searchAlbumsAPI(inputText),
+      artist: inputText,
+    });
+  };
+
   render() {
-    const { buttonDisabled, inputText } = this.state;
+    const { buttonDisabled, inputText, albums, artist } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
@@ -35,9 +48,28 @@ export default class Search extends Component {
           type="button"
           data-testid="search-artist-button"
           disabled={ buttonDisabled }
+          onClick={ this.searchAlbum }
         >
           Pesquisar
         </button>
+        <div>
+          <p>{`Resultado de álbuns de: ${artist}`}</p>
+          {albums.length === 0 ? (
+            <p>Nenhum álbum foi encontrado</p>
+          ) : (
+            albums.map((album) => (
+              <div key={ album.collectionId }>
+                <Link
+                  to={ `/album/${album.collectionId}` }
+                  data-testid={ `link-to-album-${album.collectionId}` }
+                >
+                  <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+                  <p>{album.collectionName}</p>
+                </Link>
+              </div>
+            ))
+          )}
+        </div>
       </div>
     );
   }
